4b0926a767d5f2f5af3c3da31a392e57aabb07e2
Mark Schumacher <schuma1022@gmail.com>
2022-09-15 12:25:08 -0500

    Add axios post

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,28 @@
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 function Review() {
     const history = useHistory();
     const feedback = useSelector(store => store)
+
+    const handleSubmit = () => {
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: {
+                feeling: feedback.feeling,
+                understanding: feedback.understanding,
+                support: feedback.support,
+                comments: feedback.comments
+            }
+        }).then((response) => {
+            history.push('/success')
+        }).catch((error) => {
+            console.log(error);
+            alert('Something went wrong!')
+        });
+    };
 
     return (
         <>
@@ -12,7 +31,7 @@ function Review() {
             <h3>Understanding Rating: {feedback.understanding}</h3>
             <h3>Supported Rating: {feedback.support}</h3>
             <h3>Comments: {feedback.comments}</h3>
-            <button onClick={() => history.push('/success')} >Submit</button>
+            <button onClick={handleSubmit} >Submit</button>
         </>
     );
 };
