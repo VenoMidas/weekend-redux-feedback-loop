561799bcaf97959a9d1b370c0dfb0f0d5cc13657
Mark Schumacher <schuma1022@gmail.com>
2022-09-15 12:32:28 -0500

    New Survey button clears inputs

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,14 +1,21 @@
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function Success() {
     const history = useHistory();
+    const dispatch = useDispatch();
+
+    const handleButton = () => {
+        dispatch({ type: 'CLEAR_ALL' });
+        history.push('/feeling');
+    };
 
     return (
         <>
             <h1>Thank you for your feedback!</h1>
             <h3>Your feedback has been submited successfully!</h3>
             <br />
-            <button onClick={() => history.push('/feeling')} >New Survey</button>
+            <button onClick={handleButton} >New Survey</button>
         </>
 
     );
