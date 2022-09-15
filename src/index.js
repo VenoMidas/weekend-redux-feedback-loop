e0322b12065271588aa68c2b18029e8d079de334
Mark Schumacher <schuma1022@gmail.com>
2022-09-15 08:59:43 -0500

    Setup Redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux store for reducers
+const storeInstance = createStore(
+    // reducers
+    combineReducers({
+
+    }),
+    applyMiddleware(logger) // logger for debugging/verifying proper operation
+);
+
+
+
+
+ReactDOM.render(
+    // provide the redux store for App and app contents
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
