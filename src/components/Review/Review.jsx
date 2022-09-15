297e830d3912c684706c344fba288cd789b7195a
Mark Schumacher <schuma1022@gmail.com>
2022-09-15 11:30:13 -0500

    Display feedback on review page

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,17 @@
 import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Review() {
     const history = useHistory();
+    const feedback = useSelector(store => store)
 
     return (
         <>
-            <h1>This is the Review component</h1>
+            <h1>Summary:</h1>
+            <h3>Feeling rating : {feedback.feeling}</h3>
+            <h3>Understanding Rating: {feedback.understanding}</h3>
+            <h3>Supported Rating: {feedback.support}</h3>
+            <h3>Comments: {feedback.comments}</h3>
             <button onClick={() => history.push('/')} >Submit - home</button>
         </>
     );
