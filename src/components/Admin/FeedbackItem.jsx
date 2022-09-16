49b28e2763dcbe78ab1389318d32a4f6ac3b3afb
Mark Schumacher <schuma1022@gmail.com>
2022-09-16 12:22:40 -0500

    Add prompt to confirm deletion

--- src/components/Admin/FeedbackItem.jsx
+++ src/components/Admin/FeedbackItem.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { Button } from '@mui/material';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 const FeedbackItem = ({ feedback, deleteFeedback }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <>
@@ -16,10 +33,35 @@ const FeedbackItem = ({ feedback, deleteFeedback }) => {
                             cursor: "pointer",
                         }}
                         color="error"
-                        onClick={() => deleteFeedback(feedback.id)}
+                        onClick={handleClickOpen}
                     />
                 </TableCell>
             </TableRow>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+            >
+                <DialogTitle>
+                    {"Delete this item?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        This will permanantly delete this feedback item.
+                        <br />
+                        This cannot be undone!
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Go Back</Button>
+                    <Button
+                        color="error"
+                        onClick={() => deleteFeedback(feedback.id)}
+                        autoFocus
+                    >
+                        DELETE
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
